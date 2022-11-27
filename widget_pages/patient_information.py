1946361a8fa4ef13ff3733f92c4278480cbaa924
Allan Liu <allandelong.liu@mail.utoronto.ca>
2022-11-26 23:20:11 -0500

    Save date of the ANC measurement

--- widget_pages/patient_information.py
+++ widget_pages/patient_information.py
@@ -1,6 +1,7 @@
 import sys
 import json
 import math
+import datetime
 import logging
 from PyQt6.QtWidgets import QLabel, QPushButton, QVBoxLayout, QWidget, QDialogButtonBox
 from PyQt6 import uic
@@ -26,7 +27,7 @@ class Patient:
         self.bsa = bsa
         if ancEdited:
             self.ancMeasurement.append(ancMeasurement)
-
+        print(self.ancMeasurement)
 
 class PatientInformationWindow(QWidget):
     def __init__(self):
@@ -40,6 +41,7 @@ class PatientInformationWindow(QWidget):
         self.ancEdited = False
 
         self.ancMeasurementEdit.textEdited.connect(self.valueChanged)
+        self.dateEdit.editingFinished.connect(self.valueChanged)
         self.weightEdit.textEdited.connect(self.calculateBodySurfaceArea)
         self.heightEdit.textEdited.connect(self.calculateBodySurfaceArea)
 
@@ -55,12 +57,12 @@ class PatientInformationWindow(QWidget):
 
         # TODO: Load selected patient from database
 
-        self.patient = Patient("Allan", 175, 64, 50, 2)
+        self.patient = Patient("Allan", 175, 64, 50, (2, datetime.date(2022, 1, 1)))
         self.patientLineEdit.setText(self.patient.name)
         self.weightEdit.setText(str(self.patient.weight))
         self.heightEdit.setText(str(self.patient.height))
         self.dosageEdit.setText(str(self.patient.dosage))
-        self.ancMeasurementEdit.setText(str(self.patient.ancMeasurement[-1]))
+        self.ancMeasurementEdit.setText(str(self.patient.ancMeasurement[-1][0]))
         self.calculateBodySurfaceArea()
 
     def calculateBodySurfaceArea(self):
@@ -80,10 +82,11 @@ class PatientInformationWindow(QWidget):
         weight = float(self.weightEdit.text())
         height = float(self.heightEdit.text())
         dosage = float(self.dosageEdit.text())
+        date = self.dateEdit.date().toPyDate()
         bsa = float(self.bodySurfaceAreaEdit.text())
         ancMeasurement = float(self.ancMeasurementEdit.text())
         self.patient.save(
-            name, weight, height, dosage, bsa, ancMeasurement, self.ancEdited
+            name, weight, height, dosage, bsa, (ancMeasurement, date), self.ancEdited
         )
         self.ancEdited = False
 
