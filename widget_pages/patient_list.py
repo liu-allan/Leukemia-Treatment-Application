918d88a99aea03bc8448b82679f6ec7f8387a31a
Angus Wang <wangjadehao@gmail.com>
2022-11-30 01:37:47 -0500

    cleanup step

--- widget_pages/patient_list.py
+++ widget_pages/patient_list.py
@@ -19,6 +19,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 class PatientListItem(QWidget):
+    
     def __init__(self, patient_name):
         super(PatientListItem, self).__init__()
 
@@ -36,23 +37,25 @@ class PatientListItem(QWidget):
 
         self.select_button.clicked.connect(self.showPatientInfo)
     
+
     def show(self):
         for item in [self, self.label, self.select_button]:
             item.setVisible(True)
+
 
     def hide(self):
         for item in [self, self.label, self.select_button]:
             item.setVisible(False)
     
+
     def showPatientInfo(self):
-        # lol
+        # 
         self.parent().parent().parent().parent().showPatientInformationWindow()
 
 class PatientListWindow(QWidget):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
-        # uic.loadUi("ui/patientlist.ui", self)
+    def __init__(self):
+        super().__init__()
 
         self.patients = [
             "Bill Anderson",
@@ -96,20 +99,14 @@ class PatientListWindow(QWidget):
         self.main_box_layout.addWidget(self.scroll_area)
         self.setLayout(self.main_box_layout)
 
-        # layout = QVBoxLayout()
-        # button = QPushButton("Show Patient Information")
-        # button.clicked.connect(self.showPatientInformationWindow)
 
-        # layout.addWidget(QLabel("Patient List Window"))
-        # layout.addWidget(button)
-
-        # self.setLayout(layout)
     def filterSearchItems(self, input):
         for widget in self.patient_widgets:
             if input.lower() in widget.patient_name.lower():
                 widget.show()
             else:
                 widget.hide()
+
 
     def showPatientInformationWindow(self):
         self.parent().parent().showPatientInformationWindow()
