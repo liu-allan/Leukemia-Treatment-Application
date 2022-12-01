64d122c63e6232efc00a495534e64af0588fbd8b
Angus Wang <wangjadehao@gmail.com>
2022-12-01 15:16:58 -0500

    add new patient button and improved style

--- widget_pages/patient_list.py
+++ widget_pages/patient_list.py
@@ -12,7 +12,7 @@ from PyQt6.QtWidgets import (
     QLineEdit,
 )
 from PyQt6.QtCore import Qt
-from widget_pages.toolbar import ToolBar
+from PyQt6.QtGui import QFont
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -21,9 +21,20 @@ class PatientListItem(QWidget):
     def __init__(self, patient_name):
         super(PatientListItem, self).__init__()
 
+        self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+        self.setStyleSheet(
+            "background-color: #ccccff; border-radius: 5px; padding: 10px"
+        )
+
         self.patient_name = patient_name
         self.label = QLabel(self.patient_name)
-        self.select_button = QPushButton("Patient Information")
+        self.label.setFont(QFont("Avenir", 12))
+        self.select_button = QPushButton("Select")
+        self.select_button.setFont(QFont("Avenir", 12))
+        self.select_button.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.select_button.setStyleSheet(
+            "background-color: #aaaaee; border-radius: 5px; padding: 10px"
+        )
         self.middle_spacer = QSpacerItem(
             1, 1, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
         )
@@ -46,7 +57,6 @@ class PatientListItem(QWidget):
             item.setVisible(False)
 
     def showPatientInfo(self):
-        #
         self.parent().parent().parent().parent().showPatientInformationWindow()
 
 
@@ -85,10 +95,21 @@ class PatientListWindow(QWidget):
         self.patient_widgets = []
 
         self.main_box_layout = QVBoxLayout()
+        self.search_bar_layout = QHBoxLayout()
 
         self.search_bar = QLineEdit()
         self.search_bar.setPlaceholderText("Search patients")
         self.search_bar.textChanged.connect(self.filterSearchItems)
+        self.search_bar_layout.addWidget(self.search_bar)
+
+        search_bar_spacer = QWidget()
+        search_bar_spacer.setFixedWidth(20)
+        self.search_bar_layout.addWidget(search_bar_spacer)
+
+        self.new_patient_button = QPushButton("Add Patient")
+        self.new_patient_button.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.new_patient_button.clicked.connect(self.showPatientInformationWindow)
+        self.search_bar_layout.addWidget(self.new_patient_button)
 
         self.list = QWidget()
         self.list_layout = QVBoxLayout()
@@ -112,7 +133,7 @@ class PatientListWindow(QWidget):
         self.scroll_area.setWidget(self.list)
         self.scroll_area.setWidgetResizable(True)
 
-        self.main_box_layout.addWidget(self.search_bar)
+        self.main_box_layout.addLayout(self.search_bar_layout)
         self.main_box_layout.addWidget(self.scroll_area)
         self.setLayout(self.main_box_layout)
 
