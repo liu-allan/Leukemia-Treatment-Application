c1b42fdefd42fa55a4a50a2d02ceed2177a3e69d
Allan Liu <allandelong.liu@mail.utoronto.ca>
2023-01-30 21:55:00 -0500

    Create function to call and return data from matlab model

--- matlab_script.py
+++ matlab_script.py
@@ -1,6 +1,13 @@
 import matlab.engine
-eng = matlab.engine.start_matlab()
-s = eng.genpath('Leukemia-Treatment-Project')
-eng.addpath(s, nargout=0)
-eng.main(nargout=0)
-eng.quit()
+
+def runModel(bsa, numCycles, dosage):
+    
+    eng = matlab.engine.start_matlab()
+    s = eng.genpath('Leukemia-Treatment-Project')
+    eng.addpath(s, nargout=0)
+    time, nominal_trajectory, linearized_trajectory, reactive_trajectory, anticipatory_trajectory, reactive_dosage, anticipatory_dosage = eng.runController(bsa, numCycles, dosage, nargout=7)
+    eng.quit()
+
+    return time, nominal_trajectory, linearized_trajectory, reactive_trajectory, anticipatory_trajectory, reactive_dosage, anticipatory_dosage
+
+runModel(1.71, 1.0, 50.0)
