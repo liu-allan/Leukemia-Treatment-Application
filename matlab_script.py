8b899cd0e8265b31147a94c87275675625500158
Allan Liu <allandelong.liu@mail.utoronto.ca>
2023-02-08 00:39:06 -0500

    add anc measurement field to the function

--- matlab_script.py
+++ matlab_script.py
@@ -1,13 +1,13 @@
 import matlab.engine
 
-def runModel(bsa, numCycles, dosage):
+def runModel(bsa, numCycles, dosage, ANC_measurements):
     
     eng = matlab.engine.start_matlab()
     s = eng.genpath('Leukemia-Treatment-Project')
     eng.addpath(s, nargout=0)
-    time, nominal_trajectory, linearized_trajectory, reactive_trajectory, anticipatory_trajectory, reactive_dosage, anticipatory_dosage = eng.runController(bsa, numCycles, dosage, nargout=7)
+    time, nominal_trajectory, linearized_trajectory, reactive_trajectory, anticipatory_trajectory, reactive_dosage, anticipatory_dosage = eng.runController(bsa, numCycles, dosage, ANC_measurements, nargout=7)
     eng.quit()
 
     return time, nominal_trajectory, linearized_trajectory, reactive_trajectory, anticipatory_trajectory, reactive_dosage, anticipatory_dosage
 
-runModel(1.71, 2.0, [50.0, 70.0])
+runModel(1.71, 2.0, [50.0, 70.0], [2.1, 2.0, 2.3])
