1cd7cc62d9d7e4d5ff0b86c334aa68e86a68fc54
Angus Wang <wangjadehao@gmail.com>
2023-02-09 01:09:02 -0500

    Improved patient list GUI appearance

--- widget_pages/patient_list.py
+++ widget_pages/patient_list.py
@@ -5,69 +5,109 @@ from PyQt6.QtWidgets import (
     QPushButton,
     QVBoxLayout,
     QHBoxLayout,
+    QGridLayout,
     QWidget,
     QSpacerItem,
     QSizePolicy,
     QScrollArea,
     QLineEdit,
 )
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtGui import QFont, QIcon, QPixmap
 
 logging.getLogger().setLevel(logging.INFO)
 
 
-class PatientListItem(QWidget):
+class PatientListItem(QPushButton):
     def __init__(self, patient_name, patient_id, user_id):
         super(PatientListItem, self).__init__()
 
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
+        self.setObjectName("PatientListItem")
         self.setStyleSheet(
-            "background-color: #ccccff; border-radius: 5px; padding: 10px"
+            """
+            QPushButton#PatientListItem
+            {
+                min-height: 40px;
+                background-color: #ccccff;
+                border: 1px solid #aaaaaa;
+                border-radius: 5px;
+                padding: 10px
+            }
+
+            QPushButton#PatientListItem:hover
+            {
+                background-color: #b6b6fa;
+            }
+            """
         )
+        self.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.clicked.connect(self.showPatientInfo)
 
         self.patient_name = patient_name
         self.patient_id = patient_id
         self.user_id = user_id
-        self.label = QLabel(self.patient_name)
-        self.label.setFont(QFont("Avenir", 12))
-        self.user_id_label = QLabel(self.user_id)
-        self.user_id_label.setFont(QFont("Avenir", 10))
-        self.select_button = QPushButton("Select")
-        self.select_button.setFont(QFont("Avenir", 12))
-        self.select_button.setCursor(Qt.CursorShape.PointingHandCursor)
-        self.select_button.setStyleSheet(
-            "background-color: #aaaaee; border-radius: 5px; padding: 10px"
-        )
+        self.name_label = QLabel(self.patient_name)
+        self.name_label.setFont(QFont("Avenir", 13))
+        self.name_label.setFixedWidth(300)
+
+        self.user_id_label = QLabel("Patient ID: " + self.user_id)
+        self.user_id_label.setFont(QFont("Avenir", 10, italic=True))
+        self.user_id_label.setFixedWidth(300)
+        self.user_id_label.setStyleSheet("color: #505050;")
+
         self.delete_button = QPushButton("Delete")
         self.delete_button.setFont(QFont("Avenir", 12))
         self.delete_button.setCursor(Qt.CursorShape.PointingHandCursor)
         self.delete_button.setStyleSheet(
-            "background-color: #e3735e; border-radius: 5px; padding: 10px"
+            """
+            QPushButton
+            {
+                background-color: #e35e5e;
+                border-radius: 5px;
+                padding: 10px
+            }
+
+            QPushButton:hover
+            {
+                background-color: #de3333;
+            }
+            """
         )
-        self.middle_spacer = QSpacerItem(
+        name_spacer = QSpacerItem(
+            20, 1, QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum
+        )
+        main_spacer = QSpacerItem(
             1, 1, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum
         )
 
-        self.layout = QHBoxLayout()
-        self.layout.addWidget(self.label)
-        self.layout.addItem(self.middle_spacer)
-        self.layout.addWidget(self.user_id_label)
-        self.layout.addWidget(self.select_button)
-        self.layout.addWidget(self.delete_button)
+        self.layout = QGridLayout()
+        self.layout.addWidget(self.name_label, 0, 0, 1, 1)
+        # self.layout.addItem(name_spacer, 0, 1)
+        self.layout.addWidget(self.user_id_label, 1, 0, 1, 1)
+        self.layout.addItem(main_spacer, 0, 2, 2, 1)
+        # self.layout.addWidget(self.select_button)
+        self.layout.addWidget(self.delete_button, 0, 3, 2, 1)
+        # self.layout.setColumnStretch(0, 1)
+        # self.layout.setColumnStretch(1, 1)
+        # self.layout.setColumnStretch(2, 1)
+        # self.layout.setColumnStretch(3, 10)
+        # self.layout.setColumnStretch(4, 1)
 
         self.setLayout(self.layout)
 
-        self.select_button.clicked.connect(self.showPatientInfo)
+        # self.select_button.clicked.connect(self.showPatientInfo)
         self.delete_button.clicked.connect(self.deletePatient)
 
     def show(self):
-        for item in [self, self.label, self.select_button]:
-            item.setVisible(True)
+        # for item in [self, self.label, self.select_button]:
+        #     item.setVisible(True)
+        self.setVisible(True)
 
     def hide(self):
-        for item in [self, self.label, self.select_button]:
-            item.setVisible(False)
+        # for item in [self, self.label, self.select_button]:
+        #     item.setVisible(False)
+        self.setVisible(False)
 
     def showPatientInfo(self):
         self.parent().parent().parent().parent().showPatientInformationWindow(
@@ -107,21 +147,64 @@ class PatientListWindow(QWidget):
         self.main_box_layout = QVBoxLayout()
         self.search_bar_layout = QHBoxLayout()
 
-        self.search_bar = QLineEdit()
-        self.search_bar.setPlaceholderText("Search patients")
-        self.search_bar.textChanged.connect(self.filterSearchItems)
-        self.search_bar_layout.addWidget(self.search_bar)
+        search_bar_spacer1 = QWidget()
+        search_bar_spacer1.setFixedWidth(5)
+        self.search_bar_layout.addWidget(search_bar_spacer1)
 
-        search_bar_spacer = QWidget()
-        search_bar_spacer.setFixedWidth(20)
-        self.search_bar_layout.addWidget(search_bar_spacer)
-
-        self.new_patient_button = QPushButton("Add Patient")
+        self.new_patient_button = QPushButton("+")
+        self.new_patient_button.setFont(QFont("Avenir", 15))
         self.new_patient_button.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.new_patient_button.setToolTip('Add Patient')
+        self.new_patient_button.setFixedHeight(40)
+        self.new_patient_button.setFixedWidth(40)
+        self.new_patient_button.setStyleSheet(
+            """
+            QPushButton
+            {
+                background-color: #aaaaee;
+                border-radius: 20px;
+                padding: 5px;
+            }
+
+            QPushButton:hover
+            {
+                background-color: #9898ed;
+            }
+            """
+        )
         self.new_patient_button.clicked.connect(self.showPatientFormWindow)
         self.search_bar_layout.addWidget(self.new_patient_button)
 
+        search_bar_spacer2 = QWidget()
+        search_bar_spacer2.setFixedWidth(10)
+        self.search_bar_layout.addWidget(search_bar_spacer2)
+
+        self.search_bar = QLineEdit()
+        self.search_bar.setPlaceholderText("Search patients")
+        self.search_bar.setStyleSheet(
+            """
+            QLineEdit {
+                border: 1px solid #aaaaaa;
+                border-radius: 5px;
+                padding: 5px;
+            }
+            """
+        )
+        self.search_bar.textChanged.connect(self.filterSearchItems)
+        self.search_bar_layout.addWidget(self.search_bar)
+
         self.scroll_area = QScrollArea()
+        self.scroll_area.setStyleSheet(
+            """
+            QScrollArea
+            {
+                background-color: #ffffff;
+                border: 1px solid #aaaaaa;
+                border-radius: 5px;
+                padding: 5px
+            }
+            """
+        )
         self.scroll_area.setHorizontalScrollBarPolicy(
             Qt.ScrollBarPolicy.ScrollBarAlwaysOff
         )
@@ -151,6 +234,10 @@ class PatientListWindow(QWidget):
 
     def displayPatientList(self):
         self.list = QWidget()
+        self.list.setObjectName("PatientList")
+        self.list.setStyleSheet(
+            "QWidget#PatientList { background-color: #ffffff; }"
+        )
         self.list_layout = QVBoxLayout()
 
         for patient_name, patient_id, user_id in self.patients:
