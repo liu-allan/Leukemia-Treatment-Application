47c7d85a2ad8b843556497afb92c3b8e74b61b39
Allan Liu <allandelong.liu@mail.utoronto.ca>
2023-02-26 14:24:18 -0500

    Add description for function

--- matlab_script.py
+++ matlab_script.py
@@ -1,5 +1,19 @@
 import matlab.engine
 
+"""
+bsa - Body Surface Area, float
+numCycles - Number of cycles to run
+dosage - mg
+ANC_measurements - (Absolute Neutrophil Count / Litre) x 1e9
+
+Sample Call: 
+
+runModel(1.71, 3.0, [50.0, 70.0], [2.1, 2.0, 2.3])
+
+Given 2 previous dosages and ANC measurements and one new measurement (2.3), find the anticipatory dosage.
+runModel will automatically follow the reactive dosage strategy, therefore we do not need 3 dosages.
+
+"""
 def runModel(bsa, numCycles, dosage, ANC_measurements):
     
     eng = matlab.engine.start_matlab()
@@ -10,4 +24,3 @@ def runModel(bsa, numCycles, dosage, ANC_measurements):
 
     return time, nominal_trajectory, linearized_trajectory, reactive_trajectory, anticipatory_trajectory, reactive_dosage, anticipatory_dosage
 
-runModel(1.71, 2.0, [50.0, 70.0], [2.1, 2.0, 2.3])
