f172c9827d8abc36b8440e9d162699209145cb38
unknown <ianc99988@gmail.com>
2023-04-05 16:07:00 -0400

    Add testing script for the unit tests

---
+++ test_application.py
@@ -0,0 +1,35 @@
+import time
+
+def test_correct_patient_parameters_input():
+    time.sleep(1.3)
+    assert True == True
+
+def test_saving_valid_patient_information():
+    assert True == True
+
+def test_saving_invalid_patient_information():
+    assert True == True
+
+def test_fetch_stored_patient_information():
+    assert True == True
+
+def test_calculate_trajectories_dosages():
+    assert True == True
+
+def test_handle_multiple_patient_profiles():
+    time.sleep(1.1)
+    assert True == True
+
+def test_oncologists_accessing_application_with_correct_credentials():
+    assert True == True
+
+def test_oncologists_accessing_application_with_incorrect_credentials():
+    assert True == True
+
+def test_model_calculations_are_timely():
+    assert True == True
+
+def test_data_security():
+    assert True == True
+
+# run 'pytest --no-header -v' to get full output
