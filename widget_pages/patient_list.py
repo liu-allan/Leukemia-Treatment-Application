dadf4a6eec9b56a1678f935e24ec6f1a924a4233
Angus Wang <wangjadehao@gmail.com>
2023-04-06 17:34:56 -0400

    More GUI improvements for patient list and patient form pages (#78)
    * change delete button to icon
    
    * add avatar on patient list for looks
    
    * minor changes to button looks

--- widget_pages/patient_list.py
+++ widget_pages/patient_list.py
@@ -15,7 +15,7 @@ from PyQt6.QtWidgets import (
     QMessageBox,
     QDialogButtonBox
 )
-from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtCore import Qt, QSize, QPropertyAnimation, QAbstractAnimation
 from PyQt6.QtGui import QFont, QIcon, QPixmap
 
 from datetime import datetime
@@ -33,15 +33,16 @@ class PatientListItem(QPushButton):
 
         self.setAttribute(Qt.WidgetAttribute.WA_StyledBackground, True)
         self.setObjectName("PatientListItem")
+        self.setContentsMargins(15, 0, 0, 0)
         self.setStyleSheet(
             """
             QPushButton#PatientListItem
             {
-                min-height: 70px;
+                min-height: 100px;
                 background-color: #ebebf2;
                 border: 1px solid #aaaaaa;
-                border-radius: 5px;
-                padding: 10px
+                border-radius: 30px;
+                padding: 5px
             }
 
             QPushButton#PatientListItem:hover
@@ -59,6 +60,25 @@ class PatientListItem(QPushButton):
         self.user_id = user_id
         self.is_admin = is_admin
         self.birthday = datetime.strptime(birthday, '%Y%m%d').strftime('%Y-%m-%d') if birthday else ""
+
+        self.avatar = QPushButton(self.patient_name[0])
+        self.avatar.setObjectName("avatar")
+        self.avatar.setFont(QFont("Avenir", 25))
+        self.avatar.setFixedHeight(80)
+        self.avatar.setFixedWidth(80)
+        self.avatar.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.avatar.setStyleSheet(
+            """ 
+            QPushButton#avatar
+            {
+                background-color: #aaaaee;
+                border-radius: 40px;
+                border-style: outset;
+                border: 2px solid #aaaaee;
+                padding: 10px;
+            }
+            """
+        )
 
         self.name_label = QLabel(self.patient_name)
         patient_name_font = QFont("Avenir", 18)
@@ -85,28 +105,29 @@ class PatientListItem(QPushButton):
         else:
             self.birthday_label = QLabel()
 
-        self.delete_button = QPushButton("Delete")
-        self.delete_button.setFont(QFont("Avenir", 15))
+        self.delete_button = QPushButton()
+        self.delete_button.setIcon(QIcon("icons/delete.png"))
+        self.delete_button.setIconSize(QSize(40, 40))
         self.delete_button.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.delete_button.setToolTip("Delete")
+        self.delete_button.setContentsMargins(0, 0, 10, 10)
         self.delete_button.setStyleSheet(
             """
             QPushButton
             {
-                background-color: #fa7a7a;
-                border: 1px solid #c23329;
-                border-radius: 5px;
-                color: #505050;
+                background-color: rgba(255, 255, 255, 0);
+                border-radius: 25px;
                 padding: 20px
-            }
-
-            QPushButton:hover
-            {
-                background-color: #fc5353;
-                color: #000000;
             }
             """
         )
+        self.delete_button.enterEvent = self.onButtonHover
+        self.delete_button.leaveEvent = self.onButtonUnhover
+        self.delete_button.clicked.connect(self.deletePatient)
 
+        avatar_spacer = QSpacerItem(
+            10, 1, QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum
+        )
         name_spacer = QSpacerItem(
             20, 1, QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Minimum
         )
@@ -115,16 +136,30 @@ class PatientListItem(QPushButton):
         )
 
         self.layout = QGridLayout()
-        self.layout.addWidget(self.name_label, 0, 0, 1, 1)
-        self.layout.addWidget(self.user_id_label, 1, 0, 1, 1)
-        self.layout.addItem(name_spacer, 0, 1, 2, 1)
-        self.layout.addWidget(self.birthday_label, 1, 2, 1, 1)
-        self.layout.addItem(main_spacer, 0, 3, 2, 1)
-        self.layout.addWidget(self.delete_button, 0, 4, 2, 1)
+        self.layout.addWidget(self.avatar, 0, 0, 2, 1)
+        self.layout.addItem(avatar_spacer, 0, 1, 2, 1)
+        self.layout.addWidget(self.name_label, 0, 2, 1, 1)
+        self.layout.addWidget(self.user_id_label, 1, 2, 1, 1)
+        self.layout.addItem(name_spacer, 0, 3, 2, 1)
+        self.layout.addWidget(self.birthday_label, 1, 4, 1, 1)
+        self.layout.addItem(main_spacer, 0, 5, 2, 1)
+        self.layout.addWidget(self.delete_button, 0, 6, 2, 1)
 
         self.setLayout(self.layout)
-
-        self.delete_button.clicked.connect(self.deletePatient)
+    
+    def onButtonHover(self, event):
+        self.animation = QPropertyAnimation(self.delete_button, b"iconSize")
+        self.animation.setDuration(200)
+        self.animation.setStartValue(self.delete_button.iconSize())
+        self.animation.setEndValue(QSize(46, 46))
+        self.animation.start()
+    
+    def onButtonUnhover(self, event):
+        self.animation = QPropertyAnimation(self.delete_button, b"iconSize")
+        self.animation.setDuration(200)
+        self.animation.setStartValue(self.delete_button.iconSize())
+        self.animation.setEndValue(QSize(40, 40))
+        self.animation.start()
 
     def show(self):
         self.setVisible(True)
