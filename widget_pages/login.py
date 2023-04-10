e239f141830740282f8b61698c59af74803184fe
Anne Xie <anne.xie@mail.utoronto.ca>
2023-04-09 23:16:44 -0400

    Added animation for login, changed patient form UI, changed oncologist form UI, added Gender, did not update database

--- widget_pages/login.py
+++ widget_pages/login.py
@@ -10,6 +10,7 @@ from PyQt6.QtWidgets import (
     QWidget,
     QLineEdit,
     QFrame,
+    QGraphicsOpacityEffect,
     QGraphicsBlurEffect,
     QStyle,
     QGridLayout,
@@ -18,7 +19,7 @@ from PyQt6.QtWidgets import (
     QToolBar,
 )
 from PyQt6 import uic
-from PyQt6.QtCore import Qt, QSize
+from PyQt6.QtCore import Qt, QSize, QPropertyAnimation, QParallelAnimationGroup, QPoint, QEasingCurve, QRect
 from PyQt6.QtGui import QFont, QIcon
 import qtawesome as qta
 
@@ -35,11 +36,16 @@ class LoginWindow(QWidget):
         self.setLayout(self.mainPageLayout)
 
         self.picture = QLabel()
+        anim_1 = self.slideAnimation(self.picture, -100, 0, 0, 0, 1200)
+        anim_1.setEasingCurve(QEasingCurve.Type.InOutCubic)
         self.picture.setStyleSheet('border-image: url("icons/background.png"); background-repeat: no-repeat; background-position: center; height: auto; border-radius: 20px')
         self.picture.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.mainPageLayout.addWidget(self.picture, 50)
 
         self.quotes = QLabel()
+        anim_2 = self.slideAnimation(self.quotes, -100, 300, 215, 290, 1200)
+        anim_2.setEasingCurve(QEasingCurve.Type.InOutCubic)
+
         self.quotes.setObjectName("QuotesBackground")
         self.quotes.setFrameShape(QFrame.Shape.Box)
         self.quotes.setFixedWidth(400)
@@ -58,29 +64,22 @@ class LoginWindow(QWidget):
         self.quotes.raise_()
 
         self.quotesLayout = QVBoxLayout(self.quotes)
-
         self.pictureTop = QLabel("A graphical tool in comparing two different dosage strategies used in Leukemia treatment")
         self.pictureTop.setObjectName("QuoteTop")
         self.pictureTop.setFont(QFont("Avenir", 25))
-        # self.pictureTop.setFrameShape(QFrame.Shape.Box)
         self.pictureTop.setAlignment(Qt.AlignmentFlag.AlignLeft)
         self.pictureTop.setAlignment(Qt.AlignmentFlag.AlignBottom)
-        # self.pictureTop.setAlignment(Qt.AlignmentFlag.AlignVCenter)
         self.pictureTop.setWordWrap(True)
-        # self.pictureTop.setFixedWidth(400)
-        # self.pictureTop.setFixedHeight(350)
         self.pictureTop.setStyleSheet(
             """
             QLabel#QuoteTop
             {
-                color: #5a5a5a; font-weight: bold; padding: 10px
+                color: #000000; font-weight: bold; padding: 10px
             }
             """
         )
         self.pictureTop.setContentsMargins(30, 0, 30, 0)
-        # self.pictureTop.move(215, 290)
         self.quotesLayout.addWidget(self.pictureTop, 2)
-        # self.pictureTop.raise_()
 
         self.quoteBottom = QLabel("This can help oncologists to decide what strategy should be applied better for patient's health")
         self.quoteBottom.setObjectName("QuoteBottom")
@@ -92,7 +91,7 @@ class LoginWindow(QWidget):
             """
             QLabel#QuoteBottom
             {
-                color: #5a5a5a; padding: 10px
+                color: #000000; padding: 10px
             }
             """
         )
@@ -105,7 +104,6 @@ class LoginWindow(QWidget):
             "background-color: #ffffff; border-radius: 20px;"
         )
         self.mainPageLayout.addWidget(self.login, 50)
-
         self.layout = QVBoxLayout(self.login)
         
         self.title = QWidget()
@@ -135,12 +133,21 @@ class LoginWindow(QWidget):
 
         self.layout.addSpacerItem(self.spacer)
 
+        self.middleLayout = QVBoxLayout()
+        self.middleLayout.setContentsMargins(0, 0, 0, 0)
+        self.middleFrame = QFrame()
+        self.middleFrame.setContentsMargins(0, 0, 0, 0)
+        self.middleFrame.setLayout(self.middleLayout)
+        self.layout.addWidget(self.middleFrame)
+        anim_3 = self.slideAnimation(self.middleFrame, 70, 250, 70, 300, 1200)
+        anim_3.setEasingCurve(QEasingCurve.Type.InOutCubic)
+
         self.titleLabel = QLabel("Welcome!")
         self.titleLabel.setFont(QFont("Avenir", 50))
         self.titleLabel.setContentsMargins(0, 0, 0, 40)
         self.titleLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.titleLabel.setStyleSheet("font-weight: bold;")
-        self.layout.addWidget(self.titleLabel)
+        self.titleLabel.setStyleSheet("font-weight: bold; color: #000000")
+        self.middleLayout.addWidget(self.titleLabel)
 
         self.usernameLineEdit = QLineEdit()
         self.usernameLineEdit.setFont(QFont("Avenir", 18))
@@ -151,7 +158,7 @@ class LoginWindow(QWidget):
             "background-color: #f5f5f5; height: 60px; border-radius: 20px;"
         )
         self.usernameLineEdit.setFixedWidth(500)
-        self.layout.addWidget(self.usernameLineEdit, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.middleLayout.addWidget(self.usernameLineEdit, alignment=Qt.AlignmentFlag.AlignCenter)
 
         self.passwordLineEdit = QLineEdit()
         self.passwordLineEdit.setPlaceholderText("Password")
@@ -163,12 +170,12 @@ class LoginWindow(QWidget):
         )
         self.passwordLineEdit.setFixedWidth(500)
         self.passwordLineEdit.setEchoMode(QLineEdit.EchoMode.Password)
-        self.layout.addWidget(self.passwordLineEdit, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.middleLayout.addWidget(self.passwordLineEdit, alignment=Qt.AlignmentFlag.AlignCenter)
 
         self.errorLabel = QLabel()
         self.errorLabel.setFont(QFont("Avenir", 12))
         self.errorLabel.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.layout.addWidget(self.errorLabel)
+        self.middleLayout.addWidget(self.errorLabel)
 
         self.loginPushButton = QPushButton("Login ")
         self.loginPushButton.setIcon(QIcon("icons/login.png"))
@@ -176,15 +183,25 @@ class LoginWindow(QWidget):
         self.loginPushButton.setLayoutDirection(Qt.LayoutDirection.RightToLeft)
         self.loginPushButton.clicked.connect(self.loginPushed)
         self.loginPushButton.setCursor(Qt.CursorShape.PointingHandCursor)
-        self.loginPushButton.setFixedWidth(200)
+        self.loginPushButton.setMinimumWidth(200)
+        self.loginPushButton.setMinimumHeight(45)
+        self.loginPushButton.setMaximumHeight(60)
         self.loginPushButton.setFont(QFont("Avenir", 18))
         self.loginPushButton.setShortcut("Return")
         self.loginPushButton.setStyleSheet(
-            "background-color: #aaaaee; border-radius: 20px; padding: 10px"
+            "background-color: #aaaaee; border-radius: 20px; padding: 10px; color: #000000"
         )
-        self.layout.addWidget(self.loginPushButton, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.loginPushButton.enterEvent = self.onButtonHover
+        self.loginPushButton.leaveEvent = self.onButtonUnhover
+        
+        self.middleLayout.addWidget(self.loginPushButton, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.middleLayout.addSpacerItem(self.spacer)
 
-        self.layout.addSpacerItem(self.spacer)
+        self.anim_group = QParallelAnimationGroup()
+        self.anim_group.addAnimation(anim_1)
+        self.anim_group.addAnimation(anim_2)
+        self.anim_group.addAnimation(anim_3)
+        self.anim_group.start()
 
         self.copyRightLabel = QLabel("Copyright @ 2023. LeukemiaCompare. All rights reserved.")
         self.copyRightLabel.setFont(QFont("Avenir", 12))
@@ -196,6 +213,20 @@ class LoginWindow(QWidget):
         )
 
         self.layout.addWidget(self.copyRightLabel, alignment=Qt.AlignmentFlag.AlignCenter)
+
+    def onButtonHover(self, event):
+        self.animation = QPropertyAnimation(self.loginPushButton, b"geometry")
+        self.animation.setDuration(200)
+        self.animation.setStartValue(QRect(self.loginPushButton.pos().x(), self.loginPushButton.pos().y(), self.loginPushButton.width(),self.loginPushButton.height()))
+        self.animation.setEndValue(QRect(self.loginPushButton.pos().x(), self.loginPushButton.pos().y(), self.loginPushButton.width() + 5,self.loginPushButton.height() + 5))
+        self.animation.start()
+    
+    def onButtonUnhover(self, event):
+        self.animation = QPropertyAnimation(self.loginPushButton, b"geometry")
+        self.animation.setDuration(200)
+        self.animation.setStartValue(QRect(self.loginPushButton.pos().x(), self.loginPushButton.pos().y(), self.loginPushButton.width(),self.loginPushButton.height()))
+        self.animation.setEndValue(QRect(self.loginPushButton.pos().x(), self.loginPushButton.pos().y(), self.loginPushButton.width() - 5,self.loginPushButton.height() - 5))
+        self.animation.start()
 
     def loginPushed(self):
         try:
@@ -231,6 +262,25 @@ class LoginWindow(QWidget):
             else:
                 self.parent().parent().is_admin_user = False
             self.showPatientListWindow()
+
+    def slideAnimation(self, widget, start_x, start_y, end_x, end_y, duration):
+
+        effect = QGraphicsOpacityEffect(widget)
+        widget.setGraphicsEffect(effect)
+        anim_slide = QPropertyAnimation(widget, b"pos")
+        anim_slide.setStartValue(QPoint(start_x, start_y))
+        anim_slide.setEndValue(QPoint(end_x, end_y))
+        anim_slide.setDuration(duration)
+        return anim_slide
+    
+    def opacityAnimation(self, widget, start, end, duration):
+        effect = QGraphicsOpacityEffect(widget)
+        widget.setGraphicsEffect(effect)
+        anim_opa = QPropertyAnimation(widget, b"opacity")
+        anim_opa.setStartValue(start)
+        anim_opa.setEndValue(end)
+        anim_opa.setDuration(duration)
+        return anim_opa
 
     def updateUsername(self, username):
         self.parent().parent().updateUsername(username)
--- widget_pages/oncologist_form.py
+++ widget_pages/oncologist_form.py
@@ -2,12 +2,13 @@ import bcrypt
 import logging
 import sqlite3
 
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QPropertyAnimation, QRect
 from PyQt6.QtWidgets import (
     QWidget,
     QLabel,
     QLineEdit,
-    QDialogButtonBox,
+    QRadioButton,
+    QPushButton,
     QVBoxLayout,
     QHBoxLayout,
     QSpacerItem,
@@ -22,7 +23,7 @@ class Label(QLabel):
     def __init__(self, text, width=400):
         super().__init__()
         self.setText(text)
-        self.setFont(QFont("Avenir", 15))
+        self.setFont(QFont("Avenir", 18))
         self.setFixedWidth(width)
 
 class LineEdit(QLineEdit):
@@ -48,49 +49,192 @@ class OncologistFormWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.oncologistFormLayout = QVBoxLayout()
-        self.oncologistFormLayout.setContentsMargins(0, 0, 0, 0)
+        self.oncologistFormBigLayout = QVBoxLayout()
+        self.oncologistFormBigLayout.setContentsMargins(0, 0, 0, 0)
 
-        self.usernameLabel = Label("Oncologist Username")
-        self.usernameLineEdit = LineEdit("Username")
-        self.oncologistFormLayout.addWidget(FormRow(self.usernameLabel, self.usernameLineEdit))
+        self.oncologistFormWidget = QWidget()
+        self.oncologistFormWidget.setContentsMargins(0, 0, 0, 0)
+        self.oncologistFormWidget.setFixedWidth(640)
+        self.oncologistFormWidget.setObjectName("OncologistFormOverall")
+        self.oncologistFormWidget.setStyleSheet(
+            """
+            QWidget#OncologistFormOverall
+            {
+                background-color: #ffffff;
+                border-radius: 20px;
+            }
+            """
+        )
+        self.oncologistFormBigLayout.addWidget(self.oncologistFormWidget, alignment=Qt.AlignmentFlag.AlignHCenter)
 
-        self.passwordLabel = Label("Password")
-        self.passwordEdit = LineEdit("Password")
-        self.oncologistFormLayout.addWidget(FormRow(self.passwordLabel, self.passwordEdit))
+        self.oncologistFormLayout = QVBoxLayout(self.oncologistFormWidget)
+        self.oncologistFormLayout.setContentsMargins(0, 30, 0, 10)
+
+        self.typeOfOncologistForm = Label("New Oncologist Enrollment")
+        self.typeOfOncologistForm.setContentsMargins(30, 10, 0, 10)
+        self.typeOfOncologistForm.setFont(QFont("Avenir", 30))
+        self.typeOfOncologistForm.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.typeOfOncologistForm.setFixedWidth(self.oncologistFormWidget.width() - 60)
+        self.typeOfOncologistForm.setStyleSheet("background-color: rgba(170, 170, 238, 100); height: 60px; border-radius: 20px;")
+        self.oncologistFormLayout.addWidget(self.typeOfOncologistForm, alignment=Qt.AlignmentFlag.AlignCenter)
 
         self.fullNameLabel = Label("Full Name")
-        self.fullNameEdit = LineEdit("Name")
-        self.oncologistFormLayout.addWidget(FormRow(self.fullNameLabel, self.fullNameEdit))
+        self.fullNameLabel.setContentsMargins(30, 20, 0, 0)
+        self.oncologistFormLayout.addWidget(self.fullNameLabel, alignment=Qt.AlignmentFlag.AlignBottom)
+
+        self.oncologistNameLayout = QHBoxLayout()
+        self.oncologistNameLayout.setContentsMargins(0, 0, 0, 0)
+
+        self.oncologistFirstNameLineEdit = QLineEdit()
+        self.oncologistFirstNameLineEdit.setContentsMargins(30, 0, 0, 0)
+        self.oncologistFirstNameLineEdit.setPlaceholderText("First Name")
+        self.oncologistFirstNameLineEdit.setFont(QFont("Avenir", 18))
+        self.oncologistFirstNameLineEdit.setStyleSheet(
+            "background-color: #f5f5f5; height: 40px; border-radius: 10px; padding: 0px 10px"
+        )
+        self.oncologistNameLayout.addWidget(self.oncologistFirstNameLineEdit)
+
+        self.oncologistLastNameLineEdit = QLineEdit()
+        self.oncologistLastNameLineEdit.setContentsMargins(10, 0, 30, 0)
+        self.oncologistLastNameLineEdit.setPlaceholderText("Last Name")
+        self.oncologistLastNameLineEdit.setFont(QFont("Avenir", 18))
+        self.oncologistLastNameLineEdit.setStyleSheet(
+            "background-color: #f5f5f5; height: 40px; border-radius: 10px; padding: 0px 10px"
+        )
+        self.oncologistNameLayout.addWidget(self.oncologistLastNameLineEdit)
+        self.oncologistFormLayout.addLayout(self.oncologistNameLayout)
+        
+        self.genderLabel = Label("Gender/Sex")
+        self.genderLabel.setContentsMargins(30, 20, 0, 0)
+        self.oncologistFormLayout.addWidget(self.genderLabel, alignment=Qt.AlignmentFlag.AlignBottom)
+
+        self.genderLayout = QHBoxLayout()
+        self.genderLayout.setContentsMargins(30, 0, 0, 0)
+
+        self.radioButton = QRadioButton("Male")
+        self.radioButton.setContentsMargins(0, 0, 0, 0)
+        self.radioButton.setChecked(True)
+        self.radioButton.gender = "Male"
+        self.radioButton.setFont(QFont("Avenir", 18))
+        self.radioButton.toggled.connect(self.selectedGenderType)
+        self.genderLayout.addWidget(self.radioButton)
+
+        self.radioButton = QRadioButton("Female")
+        self.radioButton.setContentsMargins(10, 0, 0, 0)
+        self.radioButton.setFont(QFont("Avenir", 18))
+        self.radioButton.gender = "Female"
+        self.radioButton.toggled.connect(self.selectedGenderType)
+        self.genderLayout.addWidget(self.radioButton)
+
+        self.oncologistFormLayout.addLayout(self.genderLayout)
+
+        self.usernameLabel = Label("Oncologist Username")
+        self.usernameLabel.setContentsMargins(30, 20, 30, 0)
+        self.oncologistFormLayout.addWidget(self.usernameLabel, alignment=Qt.AlignmentFlag.AlignBottom)
+
+        self.usernameLineEdit = QLineEdit()
+        self.usernameLineEdit.setContentsMargins(30, 0, 30, 0)
+        self.usernameLineEdit.setPlaceholderText("Username")
+        self.usernameLineEdit.setFont(QFont("Avenir", 18))
+        self.usernameLineEdit.setStyleSheet(
+            "background-color: #f5f5f5; height: 40px; border-radius: 10px; padding: 0px 10px"
+        )
+        self.oncologistFormLayout.addWidget(self.usernameLineEdit)
+
+        self.passwordLabel = Label("Password")
+        self.passwordLabel.setContentsMargins(30, 20, 30, 0)
+        self.oncologistFormLayout.addWidget(self.passwordLabel, alignment=Qt.AlignmentFlag.AlignBottom)
+
+        self.passwordEdit = QLineEdit()
+        self.passwordEdit.setContentsMargins(30, 0, 30, 0)
+        self.passwordEdit.setPlaceholderText("Password")
+        self.passwordEdit.setFont(QFont("Avenir", 18))
+        self.passwordEdit.setStyleSheet(
+            "background-color: #f5f5f5; height: 40px; border-radius: 10px; padding: 0px 10px"
+        )
+        self.oncologistFormLayout.addWidget(self.passwordEdit)
+
+        self.confirmPasswordLabel = Label("Confirm Password")
+        self.confirmPasswordLabel.setContentsMargins(30, 20, 30, 0)
+        self.oncologistFormLayout.addWidget(self.confirmPasswordLabel, alignment=Qt.AlignmentFlag.AlignBottom)
+
+        self.confirmPasswordEdit = QLineEdit()
+        self.confirmPasswordEdit.setContentsMargins(30, 0, 30, 0)
+        self.confirmPasswordEdit.setPlaceholderText("Confirm Password")
+        self.confirmPasswordEdit.setFont(QFont("Avenir", 18))
+        self.confirmPasswordEdit.setStyleSheet(
+            "background-color: #f5f5f5; height: 40px; border-radius: 10px; padding: 0px 10px"
+        )
+        self.oncologistFormLayout.addWidget(self.confirmPasswordEdit)
+        self.confirmPasswordEdit.textEdited.connect(self.checkPasswordMatch)
+
+        self.spacer = QSpacerItem(
+            1, 1, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
+        )
+        self.oncologistFormLayout.addSpacerItem(self.spacer)
+
+        self.bottomLayout = QHBoxLayout()
+        self.bottomLayout.setContentsMargins(30, 0, 30, 10)
 
         self.errorLabel = Label("")
-        self.buttonBox = QDialogButtonBox()
-        self.buttonBox.addButton(self.buttonBox.standardButtons().Cancel)
-        self.buttonBox.addButton(self.buttonBox.standardButtons().Save)
-        self.buttonBox.setFont(QFont("Avenir", 12))
-        self.buttonBox.setFixedWidth(200)
-        self.buttonBox.setStyleSheet(
-            "background-color: #aaaaee; border-radius: 5px; padding: 10px"
+        self.errorLabel.setContentsMargins(0, 0, 0, 0)
+        self.bottomLayout.addWidget(self.errorLabel, 8, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.cancelButton = QPushButton("Cancel")
+        self.cancelButton.clicked.connect(self.showPatientListWindow)
+        self.cancelButton.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.cancelButton.setMinimumWidth(100)
+        self.cancelButton.setMinimumHeight(40)
+        self.cancelButton.setMaximumHeight(50)
+        self.cancelButton.setFont(QFont("Avenir", 18))
+        self.cancelButton.setStyleSheet(
+            "background-color: #aaaaee; border-radius: 10px; padding: 10px 15px;"
         )
+        self.bottomLayout.addWidget(self.cancelButton, 1, alignment=Qt.AlignmentFlag.AlignRight)
 
-        self.buttonBox.button(self.buttonBox.standardButtons().Cancel).clicked.connect(
-            self.showPatientListWindow
+        self.saveButton = QPushButton("Save")
+        self.saveButton.clicked.connect(self.savePatientInformation)
+        self.saveButton.setCursor(Qt.CursorShape.PointingHandCursor)
+        self.saveButton.setMinimumWidth(100)
+        self.saveButton.setMinimumHeight(40)
+        self.saveButton.setMaximumHeight(50)
+        self.saveButton.setFont(QFont("Avenir", 18))
+        self.saveButton.setStyleSheet(
+            "background-color: #aaaaee; border-radius: 10px; padding: 10px 15px;"
         )
-        self.buttonBox.button(self.buttonBox.standardButtons().Save).clicked.connect(
-            self.savePatientInformation
-        )
+        self.bottomLayout.addWidget(self.saveButton, 1, alignment=Qt.AlignmentFlag.AlignRight)
 
-        self.buttonBox.setCursor(Qt.CursorShape.PointingHandCursor)
-        self.oncologistFormLayout.addWidget(FormRow(self.errorLabel, self.buttonBox))
+        self.cancelButton.enterEvent = self.onButtonHoverCancel
+        self.cancelButton.leaveEvent = self.onButtonUnhoverCancel
 
-        self.setLayout(self.oncologistFormLayout)
+        self.saveButton.enterEvent = self.onButtonHoverSave
+        self.saveButton.leaveEvent = self.onButtonUnhoverSave
 
+        self.oncologistFormLayout.addLayout(self.bottomLayout)
+        self.setLayout(self.oncologistFormBigLayout)
+
+    def selectedGenderType(self):
+        self.radioButton = self.sender()
+        if self.radioButton.isChecked():
+            print(self.radioButton.gender)
+
+    def checkPasswordMatch(self):
+        if(self.confirmPasswordEdit.text() != self.passwordEdit.text()):
+            msg = "Password does not match!"
+            self.errorLabel.setText(msg)
+            self.errorLabel.setStyleSheet("color:red")
+            logging.error(msg)
+        else:
+            msg = "Password matches!"
+            self.errorLabel.setText(msg)
+            self.errorLabel.setStyleSheet("color:green")
+            logging.info(msg)
+    
     def savePatientInformation(self):
         try:
             username = self.usernameLineEdit.text()
             assert username != ""
             password = self.encryptPassword(self.passwordEdit.text())
-            fullName = self.fullNameEdit.text()
+            fullName = self.oncologistFirstNameLineEdit.text() + " " + self.oncologistLastNameLineEdit.text()
             
             conn = self.parent().parent().getDatabaseConnection()
             conn.execute(
@@ -135,4 +279,34 @@ class OncologistFormWindow(QWidget):
     def clearForm(self):
         self.usernameLineEdit.clear()
         self.passwordEdit.clear()
-        self.fullNameEdit.clear() 
+        self.oncologistFirstNameLineEdit.clear() 
+        self.oncologistLastNameLineEdit.clear()
+        self.confirmPasswordEdit.clear()
+    
+    def onButtonHoverCancel(self, event):
+        self.animation = QPropertyAnimation(self.cancelButton, b"geometry")
+        self.animation.setDuration(200)
+        self.animation.setStartValue(QRect(self.cancelButton.pos().x(), self.cancelButton.pos().y(), self.cancelButton.width(), self.cancelButton.height()))
+        self.animation.setEndValue(QRect(self.cancelButton.pos().x(), self.cancelButton.pos().y(), self.cancelButton.width() + 5, self.cancelButton.height() + 5))
+        self.animation.start()
+    
+    def onButtonUnhoverCancel(self, event):
+        self.animation = QPropertyAnimation(self.cancelButton, b"geometry")
+        self.animation.setDuration(200)
+        self.animation.setStartValue(QRect(self.cancelButton.pos().x(), self.cancelButton.pos().y(), self.cancelButton.width(), self.cancelButton.height()))
+        self.animation.setEndValue(QRect(self.cancelButton.pos().x(), self.cancelButton.pos().y(), self.cancelButton.width() - 5, self.cancelButton.height() - 5))
+        self.animation.start()
+    
+    def onButtonHoverSave(self, event):
+        self.animation = QPropertyAnimation(self.saveButton, b"geometry")
+        self.animation.setDuration(200)
+        self.animation.setStartValue(QRect(self.saveButton.pos().x(), self.saveButton.pos().y(), self.saveButton.width(), self.saveButton.height()))
+        self.animation.setEndValue(QRect(self.saveButton.pos().x(), self.saveButton.pos().y(), self.saveButton.width() + 5, self.saveButton.height() + 5))
+        self.animation.start()
+    
+    def onButtonUnhoverSave(self, event):
+        self.animation = QPropertyAnimation(self.saveButton, b"geometry")
+        self.animation.setDuration(200)
+        self.animation.setStartValue(QRect(self.saveButton.pos().x(), self.saveButton.pos().y(), self.saveButton.width(), self.saveButton.height()))
+        self.animation.setEndValue(QRect(self.saveButton.pos().x(), self.saveButton.pos().y(), self.saveButton.width() - 5, self.saveButton.height() - 5))
+        self.animation.start()
